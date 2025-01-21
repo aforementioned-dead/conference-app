91119f9089b928e8b935e3537505e554ee02c3d6
aforementioned-dead <georg7100@gmail.com>
2025-01-22 00:44:36 +0500

    Fix tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from app.main import app
+#from fastapi import Header
 
 client = TestClient(app)
 
@@ -17,18 +18,20 @@ def test_create_room(client):
 
 
 def test_schedule_by_room(client):
-    client.post("/rooms", json={"name": "Основная комната"})
+    client.post("/users", json={"username": "Илон Макс",
+                                "role": "Докладчик"})
     client.post("/presentations", json={
         "title": "Моя первая презентация",
         "description": "Я очень волнуюсь (правда)",
         "presenter": "Илон Макс"
-    })
+    }, headers={"user-id": "1"})
+    client.post("/rooms", json={"name": "Основная комната"})
     client.post("/schedules", json={
         "room_id": 1,
         "presentation_id": 1,
         "start_time": "2025-01-16T10:00:00",
         "end_time": "2025-01-16T11:00:00"
-    })
+    }, headers={"user-id": "1"})
     response = client.get("/schedule-by-room")
     assert response.status_code == 200
     assert "Основная комната" in response.json()
@@ -37,23 +40,24 @@ def test_schedule_by_room(client):
 
 def test_time_slot_overlap(client):
     client.post("/rooms", json={"name": "Основная комната"})
+    client.post("users", json={"username": "Илон Макс", "role": "Докладчик"})
     client.post("/presentations", json={
         "title": "Моя первая презентация",
         "description": "Я очень волнуюсь (правда)",
         "presenter": "Илон Макс"
-    })
+    }, headers={"user-id": "1"})
     client.post("/schedules", json={
         "room_id": 1,
         "presentation_id": 1,
         "start_time": "2025-01-16T10:00:00",
         "end_time": "2025-01-16T11:00:00"
-    })
+    }, headers={"user-id": "1"})
     response = client.post("/schedules", json={
         "room_id": 1,
         "presentation_id": 1,
         "start_time": "2025-01-16T10:30:00",
         "end_time": "2025-01-16T11:30:00"
-    })
+    }, headers={"user-id": "1"})
     assert response.status_code == 400
     assert response.json()["detail"] == "Это время уже занято другой презентацией. Пожалуйста, выберите другое время!"
 
